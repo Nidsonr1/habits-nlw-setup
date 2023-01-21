6f693620cc9ccde0fa8f7d4d3ac4caa85c3ab9fa
Nidsonr1 <nidsoncampos@hotmail.com>
2023-01-21 20:05:46 -0300

    Aula 05

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,6 +11,6 @@ app.register(appRoutes);
 app.listen({
   port: 3333,
   host: '0.0.0.0'
-}).then(() => {
-  console.log('HTTP Server Running!')
+}).then((url) => {
+  console.log(`HTTP Server Running on ${url}!`)
 });
--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -9,7 +9,7 @@ export function ProgressBar(props: ProgressBarProps) {
         role="progressbar"
         aria-label="Progresso de hábitos completados nesse dia"
         aria-valuenow={props.progress}
-        className="h-3 rouded-xp bg-violet-600"
+        className="h-3 rouded-xp bg-violet-600 transition-all"
         style={{
           width: `${props.progress}%`
         }}
